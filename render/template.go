719988af3a4205c5f9e19bceb626c313476f76da
BobbyTables <robert@creativequeries.com>
2016-09-02 18:35:26 -0400

    Allow replacing names for vars with varListNamed
    This allows you to replace the name of the variable in a list. Making it very easier to write middlewares in your templating files.

--- render/template.go
+++ render/template.go
@@ -1,12 +1,17 @@
 package render
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"text/template"
 
 	"github.com/bobbytables/faceswap/faceswap"
 )
+
+// ErrVarCountMismatch is an error returned when varListNamed is passed a mismatch
+// of tuples vs names
+var ErrVarCountMismatch = errors.New("tuples and name count does not match")
 
 // InterfaceTemplate represents an easy-to-template value of an actual
 // Go interface.
@@ -17,7 +22,8 @@ type InterfaceTemplate struct {
 
 // RenderFuncs returns the default rendering funcs for go templates
 var RenderFuncs = template.FuncMap{
-	"varList": varList,
+	"varList":      varList,
+	"varListNamed": varListNamed,
 }
 
 func varList(tuples []faceswap.Tuple) string {
@@ -28,4 +34,17 @@ func varList(tuples []faceswap.Tuple) string {
 	}
 
 	return strings.Join(args, ", ")
+}
+
+func varListNamed(tuples []faceswap.Tuple, names ...string) (string, error) {
+	if len(tuples) != len(names) {
+		return "", ErrVarCountMismatch
+	}
+
+	var ts []faceswap.Tuple
+	for i, tuple := range tuples {
+		ts = append(ts, faceswap.Tuple{Name: names[i], Type: tuple.Type})
+	}
+
+	return varList(ts), nil
 }
--- render/template_test.go
+++ render/template_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/bobbytables/faceswap/faceswap"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestVarListFunc(t *testing.T) {
@@ -30,6 +31,30 @@ func TestVarListFunc(t *testing.T) {
 		t.Run(tc.scenario, func(t *testing.T) {
 			result := varList(tc.tuples)
 
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestVarListNamedFunc(t *testing.T) {
+	testcases := []struct {
+		scenario  string
+		expected  string
+		tuples    []faceswap.Tuple
+		namedVars []string
+	}{
+		{
+			scenario:  "Replaces an empty name with the parameter at the position",
+			expected:  "s string",
+			tuples:    []faceswap.Tuple{{Name: "", Type: types.Typ[types.String]}},
+			namedVars: []string{"s"},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.scenario, func(t *testing.T) {
+			result, err := varListNamed(tc.tuples, tc.namedVars...)
+			require.Nil(t, err)
 			assert.Equal(t, tc.expected, result)
 		})
 	}
